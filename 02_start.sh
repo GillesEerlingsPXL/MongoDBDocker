2be13a6aed5f8d5e5020d3c2d0857dfac87bec39
Gilles Eerlings <gilles.eerlings@student.pxl.be>
2021-01-04 14:35:13 +0100

    Update Docker hostname

--- 02_start.sh
+++ 02_start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --name mongo_DB --network host -p 27017:27017 -v $(pwd)/data:/data/db -it mongo:latest /bin/bash
+docker run --rm --name mongo_DB --hostname MongoDB --network host -p 27017:27017 -v $(pwd)/data:/data/db -it mongo:latest /bin/bash
